6080566b26678dde1fa6ac7f2965d4e38894c6fc
Abhidadhich69 <sharmaabhi72005@gmail.com>
2025-03-30 16:57:06 +0530

    coomited

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({origin:"https://ghiblimatic.vercel.app/"}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
